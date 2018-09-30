860bce46f06dd26b8e270614424e6554108a59dc
wq14173162 <wq14173162@sina.com>
2018-09-30 15:59:39 +0800

    init commit

---
+++ c-mall-security/src/main/java/com/mall/security/CMallSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.mall.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMallSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMallSecurityApplication.class, args);
+	}
+}
